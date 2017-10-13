24f3f6afab15fe2c163147245187a5487bdeab3e
Muhammed Essa Hameed <muhammed@fedora.muhammed>
2017-10-13 15:02:29 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
+import { Geolocation } from '@ionic-native/geolocation';
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -32,6 +36,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
+    Geolocation,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component ,ViewChild,ElementRef} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+
+declare let google;
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,73 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+@ViewChild('map') mapElement : ElementRef;
+map:any;
+  constructor(public navCtrl: NavController,
+  public geolocation:Geolocation) {
 
   }
 
+  ionViewDidLoad(){
+
+this.loadmap();
+  }
+
+
+loadmap(){
+  this.geolocation.getCurrentPosition().then((position)=>{
+
+    let latlong = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+
+    let options = {
+      center:latlong,
+      zoom:15,
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+     //mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement,options);
+
+    var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+
+    var marker = new google.maps.Marker({
+      position: latlong,
+      map: this.map,
+      title:"Hello World!",
+      label: 'Muhammed',
+      icon: {
+        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+        scale: 4
+      },
+      draggable:true,
+    });
+var mydiscriptions = "THis is my application with Ionic 3 my name is muhammed essa"
+var infowindow = new google.maps.InfoWindow({
+  content: mydiscriptions
+});
+marker.addListener('click', function() {
+  infowindow.open(this.map, marker);
+});
+
+ },(error)=>{
+      console.log(error);
+   });
+
+
+
+
 }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
